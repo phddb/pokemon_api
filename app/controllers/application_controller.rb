000eeeb684429fa5990e452c26cb95d247d59de2
d-H- <davehbenson@gmail.com>
2023-05-02 14:19:23 -0400

    light refactoring: removed dead code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,9 +25,4 @@ class ApplicationController < ActionController::API
 
   wrap_parameters format: %i[json xml url_encoded_form multipart_form]
 
-  # When required parameters are missing, strong params will throw an exception.
-  #  Catch it here and send :bad_request (400) to client
-  rescue_from ActionController::ParameterMissing do
-    render nothing: true, status: :bad_request
-  end
 end
